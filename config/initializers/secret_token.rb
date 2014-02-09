9358170f127c142e55cf12c270ea9cfcce5f7d58
Jared McFarland <jared.online@gmail.com>
2014-02-09 11:21:18 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PoliMigraRas::Application.config.secret_key_base = 'ef900a3aaaa5129381cb1d1f8d3069c852238f7011d027a3de9d1cf917a5bcc5f71a5b0f560824eff50564fd22f0249ca4feb54625d84428b6c7a385bdcd9d82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PoliMigraRas::Application.config.session_store :cookie_store, key: '_poli-migra-ras_session'
